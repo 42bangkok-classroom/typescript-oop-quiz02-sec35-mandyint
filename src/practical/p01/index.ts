e54e742d015df0f157e230b6a0e4a6d2d5af7580
Pimlada Patiphatpatawee <pxcopic@gmail.com>
2026-01-16 10:29:57 +0700

    choice

--- src/practical/p01/index.ts
+++ src/practical/p01/index.ts
@@ -1,1 +1,28 @@
-export function getEdgePosts() {}
+import axios from "axios";
+
+const API_ENDPOINT = "https://jsonplaceholder.typicode.com/posts";
+
+interface Data{
+  id: number,
+  title: string,
+}
+
+let dataObject: Data[];
+
+export async function loadData() {
+  try{
+    const res = await axios.get(API_ENDPOINT);
+    const responseData = res.data as Data[];
+    return dataObject = [...responseData].sort((a,b) => a.id - b.id);
+  }catch{
+    throw new Error("Failed");
+  }
+}
+
+export function getEdgePosts(){
+  const firstElement: string = dataObject[0];
+  const lastElement: string = dataObject[dataObject.length - 1];
+
+}
+
+getEdgePosts();
